23a0a9ac3026ea7bf15fa6b8718300ae7bfcefb3
Ron Tabachnik <rontabachnik@gmail.com>
2024-03-12 12:28:04 +0200

    implementation of new fetures

--- next.config.js
+++ next.config.js
@@ -2,3 +2,11 @@
 const nextConfig = {};
 
 module.exports = nextConfig;
+// module.exports = {
+//     webpack5: true,
+//     webpack: (config) => {
+//     //   config.resolve.fallback = { fs: false };
+//       config.externals = [...config.externals, 'bcrypt'];
+//       return config;
+//     },
+//   };
